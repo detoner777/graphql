fd6baf791c8bb6e2db199df3c53398f001042b7f
Alexey_Lisenko <detoner777@gmail.com>
2020-06-13 09:42:33 +0300

    2lesson

--- server/server/app.js
+++ server/server/app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const schema = require("../schema/schema");
 
 const app = express();
 const PORT = 3005;
 
-app.use("/graphql", graphqlHTTP({}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, (err) => {
   err ? console.log(err) : console.log("Server started!");
 });
-
-//https://www.youtube.com/watch?v=5h7-LVDoR6s&list=PLNkWIWHIRwMF2sVLwzRef0Cu5kzAOeRcu&index=2
